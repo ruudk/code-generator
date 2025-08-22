8ae1ca9584013fff744cdd44899288efcfa03731
Ruud Kamphuis <ruudk@users.noreply.github.com>
2025-08-22 10:33:33 +0200

    Tweak

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -10,6 +10,10 @@ $finder = Finder::create()
     ->in(__DIR__ . '/examples')
     ->in(__DIR__ . '/src')
     ->in(__DIR__ . '/tests')
-    ->append([__DIR__ . '/.php-cs-fixer.php']);
+    ->append([
+        __DIR__ . '/.php-cs-fixer.php',
+        __DIR__ . '/composer-dependency-analyser.php',
+        __DIR__ . '/phpstan.php',
+    ]);
 
 return PhpCsFixerConfigFactory::create(TicketSwapRuleSet::create())->setFinder($finder);
